69d2f725dda03c0a92031eaae6c6fe0b1bbb249a
ThomasAston <tjn.aston@gmail.com>
2025-03-18 18:21:59 +0000

    changing sign convention of euler angles

--- sixdrepnet/utils.py
+++ sixdrepnet/utils.py
@@ -160,31 +160,31 @@ def compute_rotation_matrix_from_ortho6d(poses):
 
 
 #input batch*4*4 or batch*3*3
-#output torch batch*3 x, y, z in radiant
-#the rotation is in the sequence of x,y,z
+#output torch batch*3 z, x, y in radiant
+#the rotation is in the sequence of z,x,y
 def compute_euler_angles_from_rotation_matrices(rotation_matrices):
     batch = rotation_matrices.shape[0]
     R = rotation_matrices
-    sy = torch.sqrt(R[:,0,0]*R[:,0,0]+R[:,1,0]*R[:,1,0])
+    sy = torch.sqrt(R[:,2,2]*R[:,2,2]+R[:,0,2]*R[:,0,2])
     singular = sy<1e-6
     singular = singular.float()
         
-    x = torch.atan2(R[:,2,1], R[:,2,2])
-    y = torch.atan2(-R[:,2,0], sy)
-    z = torch.atan2(R[:,1,0],R[:,0,0])
+    z = torch.atan2(R[:,1,0], R[:,1,1])
+    x = torch.atan2(R[:,2,1], sy)
+    y = torch.atan2(-R[:,0,2], R[:,2,2])
     
-    xs = torch.atan2(-R[:,1,2], R[:,1,1])
-    ys = torch.atan2(-R[:,2,0], sy)
-    zs = R[:,1,0]*0
+    zs = torch.atan2(-R[:,0,1], R[:,0,0])
+    xs = torch.atan2(R[:,2,1], sy)
+    ys = R[:,0,2]*0
         
     gpu = rotation_matrices.get_device()
     if gpu < 0:
         out_euler = torch.autograd.Variable(torch.zeros(batch,3)).to(torch.device('cpu'))
     else:
         out_euler = torch.autograd.Variable(torch.zeros(batch,3)).to(torch.device('cuda:%d' % gpu))
-    out_euler[:,0] = x*(1-singular)+xs*singular
-    out_euler[:,1] = y*(1-singular)+ys*singular
-    out_euler[:,2] = z*(1-singular)+zs*singular
+    out_euler[:,0] = z*(1-singular)+zs*singular
+    out_euler[:,1] = x*(1-singular)+xs*singular
+    out_euler[:,2] = y*(1-singular)+ys*singular
         
     return out_euler
 
